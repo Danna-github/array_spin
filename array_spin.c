d2b373298affeeb964a8689866b36bc229adb3d4
Danna <dleksk2250@gmail.com>
2020-08-02 23:59:02 +0900

    array_spin

---
+++ array_spin.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+	int array[5][5] = {
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+		{16, 17, 18, 19, 20},
+		{21, 22, 23, 24, 25}
+	};
+	int arrSize = 5;
+
+	for (int i = 0; i < arrSize; i++) {
+		for (int j = 0; j < arrSize; j++) {
+			printf("%d ", array[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n회전 시킨 후\n\n");
+
+	for (int j = 0; j < arrSize; j++) {
+		for (int i = arrSize-1; i >= 0; i--) {
+			printf("%d ", array[i][j]);
+		}
+		printf("\n");
+	}
+}
